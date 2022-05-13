b3caf4b6c2088db472ce39b7b9789fdb195faffe
tak223 <takwalassoued066@gmail.com>
2022-05-13 17:32:52 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieList from "./components/MovieList";
+import {mvl} from './components/Data'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Filter from "./components/Filter";
+import AddMovies from "./AddMovies";
+import {useState} from 'react';
+import   './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Descreption from "./components/Descreption";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [input,setInput] = useState('');
+  const [mvls,setVml] = useState(mvl);
+  const [rate,setRate] = useState(0) ;
+ 
+ 
+const handelAdd = (newm) => {
+  setVml([...mvls,newm])
+}
+ 
+const handleRating = (rate) => {
+  setRate(rate)
+
+  // other logic
 }
 
-export default App;
+// const filterstars =(rating)=>{
+// setVml(mvls.filter((el)=>el.rating === rating));
+//   }
+
+  return (
+    <div className="App">
+<Routes>
+<Route exact path='/' element={  
+
+<div>
+  <Filter  handleRating ={handleRating  } rating={rate}   setInput={setInput} /> 
+  <MovieList mvl={mvls}  rating={rate} setRate = {setRate} input={input} setInput={setInput}   />
+  <AddMovies handelAdd={handelAdd} />
+</div>
+
+
+
+} />
+   <Route exact path='/desc/:id' element={<Descreption mvl={mvls} />} />
+</Routes>
+
+    </div>
+   );
+} 
+
+
+export default App ;
